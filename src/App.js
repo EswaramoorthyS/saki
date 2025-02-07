f42032c3313f6f4181c07332e02b2d3b3d648a97
EswaramoorthyS <smeswaramoorthy@gmail.com>
2025-02-07 18:15:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src='https://picsum.photos/200/300' className="App-logo" alt="logo" />
+        <p>
+         Naanga Koduvai la irukoom 
+        </p>
+        <p>
+        Variable collections la dress kudupoom
+        </p>
+         <p>
+         contact by Saki Boutique
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
